1850b6216bb29644cc50c6541d798da552c5dc10
avikmasanta <117199753+avikmasanta@users.noreply.github.com>
2024-04-01 02:39:03 +0530

    Add files via upload
    Java Introduction / Java Stdin and Stdout I

---
+++ Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Solution {
+public static void main(String[] args) {
+Scanner avik = new Scanner(System.in);
+int a = avik.nextInt();
+int b = avik.nextInt();
+int c = avik.nextInt();
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+ }
+}
+                                                                    
